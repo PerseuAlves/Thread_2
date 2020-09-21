5249120eeb1774641e91305d55f46b2e0ee1846f
Perse <Perse@PC>
2020-09-20 23:14:35 -0300

    Atividade acadêmica sobre Threads

--- src/view/Main.java
+++ src/view/Main.java
@@ -5,8 +5,8 @@ import controller.ThreadId;
 public class Main {
 
 	public static void main(String[] args) {
-		
-		for(int i=0;i<5;i++) {
+
+		for (int i = 0; i < 5; i++) {
 			Thread ThreadId = new ThreadId();
 			ThreadId.start();
 		}
